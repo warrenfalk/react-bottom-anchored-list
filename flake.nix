74c1d861e9f74d804f2dcf0bfae4e0918372a68c
Warren Falk <warren@warrenfalk.com>
2026-04-22 15:35:27 -0400

    initial env

---
+++ flake.nix
@@ -0,0 +1,55 @@
+{
+  description = "Bootstrap shell for building a React web component";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+  };
+
+  outputs = { nixpkgs, ... }:
+    let
+      systems = [
+        "aarch64-darwin"
+        "x86_64-darwin"
+        "aarch64-linux"
+        "x86_64-linux"
+      ];
+
+      forAllSystems = f:
+        nixpkgs.lib.genAttrs systems (system:
+          let
+            pkgs = import nixpkgs { inherit system; };
+          in
+          f pkgs
+        );
+    in
+    {
+      formatter = forAllSystems (pkgs: pkgs.nixfmt-rfc-style);
+
+      devShells = forAllSystems (pkgs: {
+        default = pkgs.mkShell {
+          packages = with pkgs; [
+            git
+            jq
+            nodejs_22
+            pnpm
+          ];
+
+          shellHook = ''
+            export npm_config_fund=false
+            export npm_config_audit=false
+
+            if [ ! -f package.json ]; then
+              cat <<'EOF'
+              React component workspace is ready.
+
+              Suggested bootstrap:
+                pnpm create vite . --template react-ts
+                pnpm install
+                pnpm dev
+              EOF
+            fi
+          '';
+        };
+      });
+    };
+}
